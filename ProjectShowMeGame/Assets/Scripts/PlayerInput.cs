319c53a2131c1527aa9825d87f487946c5386393
StijnHendriks24 <stijnhendriks24@gmail.com>
2022-10-31 15:43:20 +0100

    Follow camera that rotates with player (optional)

--- ProjectShowMeGame/Assets/Scripts/PlayerInput.cs
+++ ProjectShowMeGame/Assets/Scripts/PlayerInput.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PlayerInput : MonoBehaviour
 {
+    public GameObject cameraObject;
+
     private float throttleInput = 0f;
     private float steerInput = 0f;
     private CarController carController;
@@ -19,5 +21,26 @@ public class PlayerInput : MonoBehaviour
         steerInput = Input.GetAxisRaw("Horizontal");
 
         carController.GiveInput(throttleInput, steerInput);
+
+        // TODO: Properly implement screenshake
+        if (Input.GetKeyDown(KeyCode.Space))
+            StartCoroutine(Shake(0.15f, 0.1f));
+    }
+
+    public IEnumerator Shake(float duration, float magnitude)
+    {
+        Vector3 orignalPosition = cameraObject.transform.localPosition;
+        float elapsed = 0f;
+
+        while (elapsed < duration)
+        {
+            float x = Random.Range(-1f, 1f) * magnitude;
+            float y = Random.Range(-1f, 1f) * magnitude;
+
+            cameraObject.transform.localPosition = new Vector3(x, y, orignalPosition.z);
+            elapsed += Time.deltaTime;
+            yield return 0;
+        }
+        cameraObject.transform.localPosition = orignalPosition;
     }
 }
