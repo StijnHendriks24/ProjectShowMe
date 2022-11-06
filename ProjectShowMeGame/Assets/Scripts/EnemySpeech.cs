d6768bf6598e3b7b70f1943a26a4a0e85723d77d
StijnHendriks24 <stijnhendriks24@gmail.com>
2022-11-06 14:29:21 +0100

    Implemented AI car models

--- ProjectShowMeGame/Assets/Scripts/EnemySpeech.cs
+++ ProjectShowMeGame/Assets/Scripts/EnemySpeech.cs
@@ -5,16 +5,10 @@ using UnityEngine.UI;
 
 public class EnemySpeech : MonoBehaviour
 {
-    public Image iconImage;
-    public Sprite[] speechSprites;
+    public GameObject speechBubble;
     public Vector2 minMaxChangeTime = new Vector2(2, 5);
 
     private float nextChangeIn;
-
-    void Start()
-    {
-        ChangeSpeech();
-    }
 
     void Update()
     {
@@ -22,19 +16,13 @@ public class EnemySpeech : MonoBehaviour
         rotation.y = 0f;
         rotation.z = 0f;
 
-        transform.rotation = Quaternion.Euler(rotation);
-        //transform.LookAt(transform.position + Camera.main.transform.rotation * Vector3.back, Camera.main.transform.rotation * Vector3.up);
+        //transform.rotation = Quaternion.Euler(rotation);
+        transform.LookAt(transform.position + Camera.main.transform.rotation * Vector3.back, Camera.main.transform.rotation * Vector3.up);
 
         nextChangeIn -= Time.deltaTime;
         if(nextChangeIn <= 0)
         {
-            ChangeSpeech();
+            speechBubble.SetActive(!speechBubble.activeInHierarchy);
         }
-    }
-
-    void ChangeSpeech()
-    {
-        nextChangeIn = Random.Range(minMaxChangeTime.x, minMaxChangeTime.y);
-        iconImage.sprite = speechSprites[Random.Range(0, speechSprites.Length)];
     }
 }
